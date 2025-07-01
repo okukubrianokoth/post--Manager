7b24eb39abe5199670ae45339c5c77e26b34a229
brian okoth okuku <okukubrian743@gmail.com>
2025-07-01 22:59:18 +0300

    chande url

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // Base URL for json-server API
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://json-server-mqlu.onrender.com";
 
 // Global variables
 let posts = [];          // Array to store all posts
